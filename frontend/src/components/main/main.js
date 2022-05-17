d2b43b54706dde9f9b58bd6170cd71755adefa84
YuraSab <79487730+YuraSab@users.noreply.github.com>
2022-05-17 19:16:58 +0300

    Main.js

--- frontend/src/components/main/main.js
+++ frontend/src/components/main/main.js
@@ -1,4 +1,5 @@
-import React, { Component } from "react";
+import React, { Component, useCallback, useEffect, useState } from "react";
+import "./main.css";
 
 // class Main extends Component {
 //     render() {
@@ -23,9 +24,47 @@ import React, { Component } from "react";
 
 
 const Main = () => {
+
+    const [id, setId] = useState(2);
+
+    const [film, setFilm] = useState(null);
+
+
+
+
+    useEffect(() => {
+        fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
+            .then(res => res.json())
+            .then(data => setFilm(data))
+
+    }, [id])
+
+
+
+
+    /*
+        useEffect(() => {
+        //fetchMovies();
+        
+        // fetch(`https://jsonplaceholder.typicode.com/todos/${id}`)
+        fetch(`http://localhost:8800/api/movies/find/${id}`)
+            .then(res => res.json())
+            .then(data => setFilm(data))
+
+
+    }, []);
+    */
+
+
     return (
         <div>
-            <h2>Main</h2>
+            <button onClick={() => id <= 1 ? setId(4) : setId(id => id-1) }> prev </button>
+                <h2>
+                    {
+                        film && <div> {film.name} </div>
+                    }
+                </h2>
+            <button onClick={() => id >= 5 ? setId(1) : setId(id => id+1) }> next </button>
         </div>
     )
 }
